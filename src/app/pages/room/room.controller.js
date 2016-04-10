7974b06a972607a6e159d24e4245af886852721c
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-04-10 13:40:53 +0900

    Implement periodic getting messages

--- src/app/pages/room/room.controller.js
+++ src/app/pages/room/room.controller.js
@@ -1,23 +1,40 @@
 export class RoomController {
-  constructor(Restangular) {
+  constructor(Restangular, $interval) {
     'ngInject';
     this.Restangular = Restangular;
+    this.$interval = $interval;
     this.Restangular.one('rooms/1').post().then((data) => {
       this.user = data.animal;
       this.Restangular.one('rooms/1/messages').get({
         'since': 0
       }).then((msg_data) => {
         console.log(msg_data.items);
-        this.items = msg_data.items
+        this.items = msg_data.items;
+
+        this.periodicGetMessages = this.$interval(() => {
+          this.Restangular.one('rooms/1/messages').get({
+            'since': 0
+          }).then((new_msg_data) => {
+            console.log(new_msg_data.items);
+            if (this.items.length < new_msg_data.items.length){
+               this.items = new_msg_data.items;
+              this.scrollToBottom();
+            }
+          });
+        }, 2000)
       });
     });
+  }
 
+  scrollToBottom(){
     $('body').delay(100).animate({
       scrollTop: $(document).height()
     }, 500);
   }
 
   sendMessage() {
+    var sendMessage = this.messageText;
+    this.messageText = "";
     console.log(this.messageText);
     //this.items.push(
     //  {
@@ -29,10 +46,10 @@ export class RoomController {
     var date = new Date();
     var timestamp = date.getTime();
 
-    this.Restangular.one('rooms/1/messages').post('','',
+    this.Restangular.one('rooms/1/messages').post('', '',
       {
         'animal': this.user,
-        'message': this.messageText,
+        'message': sendMessage,
         'time': timestamp
       }).then(() => {
       this.Restangular.one('rooms/1/messages').get({
@@ -40,16 +57,15 @@ export class RoomController {
       }).then((msg_data) => {
         console.log(msg_data.items);
         this.items = msg_data.items;
-        this.messageText = "";
-    $('body').delay(100).animate({
-      scrollTop: $(document).height()
-    }, 500);
+        $('body').delay(100).animate({
+          scrollTop: $(document).height()
+        }, 500);
       });
     });
   }
 
   transTime(unix_time) {
-    return moment(unix_time*1000).format('HH:mm');
+    return moment(unix_time).format('HH:mm');
   }
 
 }
