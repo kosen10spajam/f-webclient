8edd7706328dc2ca20d674a9d057b5c19c496ac7
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-04-10 13:10:43 +0900

    Implement chat-room layout and sending request

--- src/app/pages/room/room.controller.js
+++ src/app/pages/room/room.controller.js
@@ -2,15 +2,54 @@ export class RoomController {
   constructor(Restangular) {
     'ngInject';
     this.Restangular = Restangular;
-    this.Restangular.one('').get().then((data) => {
-      this.msg = data.msg;
-    });
-    this.items = [];
-    for (var i = 0; i < 20; i++) {
-      this.items.push({
-        'animal': 'nyan',
-        'message': 'hello'
+    this.Restangular.one('rooms/1').post().then((data) => {
+      this.user = data.animal;
+      this.Restangular.one('rooms/1/messages').get({
+        'since': 0
+      }).then((msg_data) => {
+        console.log(msg_data.items);
+        this.items = msg_data.items
       });
-    }
+    });
+
+    $('body').delay(100).animate({
+      scrollTop: $(document).height()
+    }, 500);
   }
+
+  sendMessage() {
+    console.log(this.messageText);
+    //this.items.push(
+    //  {
+    //    'animal': this.user,
+    //    'time': moment(),
+    //    'message': this.messageText
+    //  }
+    //);
+    var date = new Date();
+    var timestamp = date.getTime();
+
+    this.Restangular.one('rooms/1/messages').post('','',
+      {
+        'animal': this.user,
+        'message': this.messageText,
+        'time': timestamp
+      }).then(() => {
+      this.Restangular.one('rooms/1/messages').get({
+        'since': 0
+      }).then((msg_data) => {
+        console.log(msg_data.items);
+        this.items = msg_data.items;
+        this.messageText = "";
+    $('body').delay(100).animate({
+      scrollTop: $(document).height()
+    }, 500);
+      });
+    });
+  }
+
+  transTime(unix_time) {
+    return moment(unix_time*1000).format('HH:mm');
+  }
+
 }
